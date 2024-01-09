4e40ca71d250c41c5c4159db1eccd52974fd80b4
Lucas Ribeiro <54214156+zeer0e1@users.noreply.github.com>
2024-01-09 11:07:33 -0300

    update
    Realizando algumas melhorias de tempo no código

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver as web
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from openpyxl import load_workbook
@@ -7,8 +6,6 @@ import os
 import pyautogui as g
 
 options = Options()
-browser = web.Chrome()
-browser.get('https://buscacepinter.correios.com.br/app/endereco/index.php?t')
 
 # browser.find_element(By.ID, "endereco").send_keys("18078458")
 
@@ -16,22 +13,20 @@ nome_arquivo_cep = "C:\cep-correios\pesquisa_cep.xlsx"
 planilha_dados = load_workbook(nome_arquivo_cep)
 shet_selecionada = planilha_dados["CEP"]
 
+browser = web.Chrome()
+browser.get('https://buscacepinter.correios.com.br/app/endereco/index.php?t')
 
-for linha in range(2, len(shet_selecionada['A']) + 1):
+for linha in range(1, len(shet_selecionada['A']) + 1 ):
     cep = shet_selecionada['A%s' % linha].value
     if cep is not None:
-
         browser.find_element(By.NAME, "endereco").send_keys(cep)
-        g.sleep(2)
+
         browser.find_element(By.NAME, 'btn_pesquisar').click()
         g.sleep(2)
-
-        rua = browser.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr[2]/td[1]')[0].text
+        rua = browser.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[1]')[0].text
         bairro = browser.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr[1]/td[2]')[0].text
         cidade = browser.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr[1]/td[3]')[0].text
-        cep = browser.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr[3]/td[4]')[0].text
-        g.sleep(2)
-
+        cep = browser.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[4]')[0].text
         sheet_dados = planilha_dados["Dados"]
         linha_corrente = len(sheet_dados['A']) + 1
 
@@ -44,11 +39,12 @@ for linha in range(2, len(shet_selecionada['A']) + 1):
         sheet_dados[colunaB] = bairro
         sheet_dados[colunaC] = cidade
         sheet_dados[colunaD] = cep
+        g.sleep(2)
         browser.find_element(By.ID, 'btn_nbusca').click()
-# Importando o elemento By
 
+    if len(shet_selecionada['A']) == 1:
+        break
 
-g.sleep(4)
 
 
 planilha_dados.save(filename=nome_arquivo_cep)
